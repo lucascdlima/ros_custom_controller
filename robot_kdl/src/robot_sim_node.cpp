f7f8748263fc190f034fd4ebd7c8db66d99e2827
lucascdlima <lucascdlima@gmail.com>
2020-08-24 09:48:42 -0300

    Adds a simple logic to reduce rate for publishing joints states.

--- robot_kdl/src/robot_sim_node.cpp
+++ robot_kdl/src/robot_sim_node.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "robot_sim_node");
   ros::NodeHandle nh;
-  ros::Rate loop_rate(100);
+  ros::Rate loop_rate(200);
 
 
   RobotKDL myrobot("/home/lucaslima/catkin_test_ws/src/robot_kdl/kuka_urdf/kuka_urdf_test.urdf", nh);
@@ -56,10 +56,12 @@ int main(int argc, char **argv)
   Eigen:: Matrix<double,7,1> q_des;
   Eigen:: Matrix<double,7,1> dq_des;
   Eigen:: Matrix<double,7,1> ddq_des;
+  bool spin = false;
 
   ROS_INFO("Node loop started");
   while (ros::ok())
   {
+
 
     /*
     // Computing desired joints positions for computed torque control
@@ -89,7 +91,7 @@ int main(int argc, char **argv)
     jnt_acc = myrobot.UpdateDynamic();
 
 
-  /*  KDL::JntSpaceInertiaMatrix M(myrobot.num_joints);
+    /*  KDL::JntSpaceInertiaMatrix M(myrobot.num_joints);
     chain_dynamics.JntToMass(jnt_pos,M);
     KDL::JntArray C(myrobot.num_joints);
     chain_dynamics.JntToCoriolis(jnt_pos,jnt_vel,C);
@@ -102,30 +104,38 @@ int main(int argc, char **argv)
     etime = now_time.toSec() - last_time.toSec();
     last_time = now_time;
 
-   /* jnt_vel.data = jnt_vel.data + jnt_acc.data*etime;
+    /* jnt_vel.data = jnt_vel.data + jnt_acc.data*etime;
     jnt_pos.data = jnt_pos.data + jnt_vel.data*etime;*/
 
-   //Updating joints velocities and position (Euler integration)
-   jnt_vel.data = jnt_acc.data*etime + myrobot.GetJointsVelocity().data;
-   jnt_pos.data = jnt_vel.data*etime + myrobot.GetJointsPosition().data;
+    //Updating joints velocities and position (Euler integration)
+    jnt_vel.data = jnt_acc.data*etime + myrobot.GetJointsVelocity().data;
+    jnt_pos.data = jnt_vel.data*etime + myrobot.GetJointsPosition().data;
 
-   // Setting new joints positions and velocities
-   myrobot.SetJointsPosition(jnt_pos.data);
-   myrobot.SetJointsVelocity(jnt_vel.data);
+    // Setting new joints positions and velocities
+    myrobot.SetJointsPosition(jnt_pos.data);
+    myrobot.SetJointsVelocity(jnt_vel.data);
 
-   ROS_INFO("Calculos realizados");
+    //ROS_INFO("Calculos realizados");
 
-   sensor_msgs::JointState states_msg;
-   myrobot.SetJointStatesMsg(states_msg);
-   joints_states_pub.publish(states_msg);
+    //temporary logic to reduce by half publication rate
+    if(!spin)
+    {
+      sensor_msgs::JointState states_msg;
+      myrobot.SetJointStatesMsg(states_msg);
+      joints_states_pub.publish(states_msg);
+
+      spin = !spin;
+    }
+    else
+      spin = !spin;
 
     ros::spinOnce();
     loop_rate.sleep();
 
-    // cout<<"Acceleration = "<<endl<<jnt_acc.data<<endl;
-     //cout<<"etime = "<<etime<<endl;
-    // cout<<"Effort = "<<endl;
-     //cout<<jnt_effort.data<<endl;
+    cout<<"Acceleration = "<<endl<<jnt_acc.data<<endl;
+    //cout<<"etime = "<<etime<<endl;
+    //cout<<"Effort = "<<endl<<myrobot.jnts_effort_command.data;
+    //cout<<jnt_effort.data<<endl;
   }
 
   joints_states_pub.shutdown();
