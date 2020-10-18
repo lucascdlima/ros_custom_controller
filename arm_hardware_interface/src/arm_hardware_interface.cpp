2743b33e74086f68cc1cc390b8f50fc4de6e5728
lucascdlima <lucascdlima@gmail.com>
2020-10-18 20:06:07 -0300

    Removes unused attributes joint_position_command_ and joint_velocity_command_.
    Changes comments format.

--- arm_hardware_interface/src/arm_hardware_interface.cpp
+++ arm_hardware_interface/src/arm_hardware_interface.cpp
@@ -8,7 +8,7 @@ ManipulatorRobot::ManipulatorRobot(ros::NodeHandle& nh) : nh_(nh)
 
 bool ManipulatorRobot::init()
 {
-  //Get the joints names set as a ros parameter of our robot hardware interface
+  //Get the ros param joints names (joints to be controlled)
   if (!nh_.getParam("/arm_robot/hardware_interface/joints", joint_names_)){
     ROS_ERROR("Could not find joints names");
     return false;
@@ -21,14 +21,12 @@ bool ManipulatorRobot::init()
   joint_position_.resize(num_jnts_);
   joint_velocity_.resize(num_jnts_);
   joint_effort_.resize(num_jnts_);
-  joint_position_command_.resize(num_jnts_);
-  joint_velocity_command_.resize(num_jnts_);
   joint_effort_command_.resize(num_jnts_);
   joints_states.resize(num_jnts_);
 
   for (unsigned int i = 0; i < num_jnts_; ++i)
   {
-    // Creates joint state handle and provide refecente to joints states and command vectors
+    // Creates joint state handle and provide reference to joints states and command vectors
     hardware_interface::JointStateHandle jointStateHandle(joint_names_[i], &joint_position_[i], &joint_velocity_[i], &joint_effort_[i]);
 
     // Creates joint handle, passes effort command vector reference and registers it in a EffortJointInterface
@@ -45,6 +43,13 @@ bool ManipulatorRobot::init()
 
 }
 
+/**
+ * @brief ManipulatorRobot::read Overloaded method to read data from hardware interface (robot).
+ * Get joints positons, velocities and efforts from hardware interface, set respective values to internal attributes and send to
+ * ros controller.
+ * @param etime ros elapsed time since controller start.
+ * @param duration ros duration time between each call of read method.
+ */
 void ManipulatorRobot::read(const ros::Time &etime, const ros::Duration &duration)
 {
   //Pass joints data from joints handles (ros hardware interface) to internal joints states vectors
@@ -56,6 +61,13 @@ void ManipulatorRobot::read(const ros::Time &etime, const ros::Duration &duratio
 
 }
 
+/**
+ * @brief ManipulatorRobot::write Overloaded method to write data (commands) to hardware interface (robot).
+ * Gets the effort commands sent from ros node controller and sends to hardware by ros topic publish interface.
+ * @param etime ros elapsed time since controller start.
+ * @param duration ros duration time between each call of write method.
+ * @param pub ros Publisher object to send messages to hardware through network.
+ */
 void ManipulatorRobot::write(const ros::Time &etime, const ros::Duration &duration, ros::Publisher &pub)
 {
   std_msgs::Float64MultiArray msg;
@@ -69,6 +81,12 @@ void ManipulatorRobot::write(const ros::Time &etime, const ros::Duration &durati
   pub.publish(msg);
 }
 
+/**
+ * @brief ManipulatorRobot::SubJointsStates Ros subscriber callback.
+ * Method called each time a message with hardware joint states is received from a ros topic. After joints states
+ * are received the values are passed to ros controller through read method call.
+ * @param msg Message received with joints states (effort, position and velocities)
+ */
 void ManipulatorRobot::SubJointsStates(const sensor_msgs::JointStateConstPtr& msg)
 {
   //Get joints states received from simulated hardware through ROS topic
